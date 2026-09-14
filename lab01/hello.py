15c5ea4eb2fc93d62f8ee2849926768d0e68180d
Yaroslav-Hnatiuk <c00325785@setu.ie>
2026-09-14 10:42:12 +0000

    cosmetic change

--- lab01/hello.py
+++ lab01/hello.py
@@ -1,6 +1,7 @@
 name = input("What is your name?: ")
 course = input("Whatchu studying?: ")
 favourite_area = input("What are of cybersecurity are you the most insterested in?: ")
+
 print("Hello, " + name + "!")
 print("You're studying " + course + ", What a surprise.")
 print("And you're interested in " + favourite_area)
